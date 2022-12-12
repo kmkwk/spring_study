88fa97fdedd7f427fa6a6095b887d9a182d7e850
김경민 <nutmy52@naver.com>
2022-12-12 23:50:51 +0900

    template callback pattern
    템플릿 콜백 패턴

---
+++ src/test/java/com/study/spa/template/Callback.java
@@ -0,0 +1,7 @@
+package com.study.spa.template;
+
+public interface Callback {
+
+    void call();
+
+}
---
+++ src/test/java/com/study/spa/template/TemplateCallbackTest.java
@@ -0,0 +1,34 @@
+package com.study.spa.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateCallbackTest {
+
+    @Test
+    void callbackV1(){
+        TimeLogTemplate timeLogTemplate = new TimeLogTemplate();
+        timeLogTemplate.execute(new Callback() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+
+        timeLogTemplate.execute(new Callback() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+
+    @Test
+    void callbackV2(){
+        TimeLogTemplate timeLogTemplate = new TimeLogTemplate();
+        timeLogTemplate.execute(() -> log.info("비즈니스 로직1 실행"));
+
+        timeLogTemplate.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+}
